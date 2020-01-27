7f65e82a0b9b9175c0f13d46438236a51c728f61
Martin <martin.samanek@oriflame.com>
2020-01-27 13:33:27 +0100

    JWT test fix

--- tests/Oriflame.PolicyBuilder.Xml.Tests/Definitions/Common/JwtValidationPolicyTests.cs
+++ tests/Oriflame.PolicyBuilder.Xml.Tests/Definitions/Common/JwtValidationPolicyTests.cs
@@ -8,13 +8,28 @@ namespace Oriflame.PolicyBuilder.Xml.Tests.Definitions.Common
     public class JwtValidationPolicyTests
     {
         [Theory]
-        //[InlineData("https://openIdConfigUrl.com", new[] { "https://issuer1.com", "https://issuer2.com" })]
-        [InlineData(null, null, "<validate-jwt />")]
-        public void CreatesCorrectPolicy(string openIdConfigUrl, string[] issuers, string expected)
+        [InlineData(null, null)]
+        public void CreatesCorrectPolicy(string openIdConfigUrl, string[] issuers)
         {
             var basePolicy = new JwtValidationPolicy(new Dictionary<string, string>(), openIdConfigUrl, issuers);
             var xml = basePolicy.GetXml().ToString();
-            xml.Should().Be(expected);
+            xml.Should().Be("<validate-jwt />");
+        }
+
+        [Theory]
+        [InlineData("https://openIdConfigUrl.com", new[] { "https://issuer1.com", "https://issuer2.com" })]
+        public void CreatesCorrectComplexPolicy(string openIdConfigUrl, string[] issuers)
+        {
+            var basePolicy = new JwtValidationPolicy(new Dictionary<string, string>(), openIdConfigUrl, issuers);
+            var xml = basePolicy.GetXml().ToString();
+            xml.Should().Be(
+$@"<validate-jwt>
+  <openid-config url=""https://openIdConfigUrl.com"" />
+  <issuers>
+    <issuer>https://issuer1.com</issuer>
+    <issuer>https://issuer2.com</issuer>
+  </issuers>
+</validate-jwt>");
         }
     }
 }
