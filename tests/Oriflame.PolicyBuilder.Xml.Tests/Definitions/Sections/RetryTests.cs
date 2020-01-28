2856d1ca6da3ce12daf52094729f34a913c13a4e
Martin <martin.samanek@oriflame.com>
2020-01-28 13:18:56 +0100

    Sections tests

---
+++ tests/Oriflame.PolicyBuilder.Xml.Tests/Definitions/Sections/RetryTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FluentAssertions;
+using Oriflame.PolicyBuilder.Xml.Definitions.Sections;
+using Xunit;
+
+namespace Oriflame.PolicyBuilder.Xml.Tests.Definitions.Sections
+{
+    public class RetryTests
+    {
+        [Theory]
+        [InlineData("@(0 == 1)", 4, 5, true)]
+        public void CreatesCorrectPolicy(string condition, int count, int intervalSeconds, bool firstFastRetry)
+        {
+            var basePolicy = new RetryPolicy(condition, count, TimeSpan.FromSeconds(intervalSeconds), firstFastRetry);
+            var xml = basePolicy.GetXml().ToString();
+            xml.Should().Be($@"<retry condition=""{condition}"" count=""{count}"" interval=""{intervalSeconds}"" first-fast-retry=""{ (firstFastRetry ? "true" : "false") }"" />");
+        }
+    }
+}
---
+++ tests/Oriflame.PolicyBuilder.Xml.Tests/Definitions/Sections/XmlOperationPolicyTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Oriflame.PolicyBuilder.Xml.Definitions.Sections;
+using Xunit;
+
+namespace Oriflame.PolicyBuilder.Xml.Tests.Definitions.Sections
+{
+    public class XmlOperationPolicyTests
+    {
+        [Fact]
+        public void CreatesCorrectPolicy()
+        {
+            var basePolicy = new XmlOperationPolicy();
+            var xml = basePolicy.GetXml().ToString();
+            xml.Should().Be(
+                @"<policies>
+  <inbound />
+  <backend />
+  <outbound />
+  <on-error />
+</policies>");
+        }
+    }
+}
