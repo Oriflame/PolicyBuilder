77e86f196620df1cd95fae02fb9811ad455b8795
Ondřej Škrabánek <ondrej.skrabanek@oriflame.com>
2021-11-10 22:44:47 +0000

    Retry policy update to accept strings

--- src/Oriflame.PolicyBuilder.Xml/Definitions/Sections/RetryPolicy.cs
+++ src/Oriflame.PolicyBuilder.Xml/Definitions/Sections/RetryPolicy.cs
@@ -6,11 +6,43 @@ namespace Oriflame.PolicyBuilder.Xml.Definitions.Sections
 {
     public class RetryPolicy : SectionPolicy
     {
-        public RetryPolicy(string condition, int count, TimeSpan interval, bool? firstFastRetry = null) : base("retry")
+        public RetryPolicy(string condition, int count, TimeSpan interval, bool? firstFastRetry = null)
+            : this(condition, count.ToString(), interval.GetSeconds(), firstFastRetry)
+        {
+        }
+
+        public RetryPolicy(string condition, string count, TimeSpan interval, bool? firstFastRetry = null)
+            : this(condition, count, interval.GetSeconds(), firstFastRetry)
+        {
+        }
+
+        public RetryPolicy(string condition, int count, string interval, bool? firstFastRetry = null)
+            : this(condition, count.ToString(), interval, firstFastRetry)
+        {
+        }
+
+        public RetryPolicy(string condition, int count, TimeSpan interval, string firstFastRetry = null)
+            : base("retry")
+        {
+            if (bool.TryParse(firstFastRetry, out bool firstFastRetryBool))
+            {
+                Initialize(condition, count.ToString(), interval.GetSeconds(), firstFastRetryBool);
+            }
+
+            Initialize(condition, count.ToString(), interval.GetSeconds(), null);
+        }
+
+        public RetryPolicy(string condition, string count, string interval, bool? firstFastRetry = null)
+            : base("retry")
+        {
+            Initialize(condition, count, interval, firstFastRetry);
+        }
+
+        private void Initialize(string condition, string count, string interval, bool? firstFastRetry = null)
         {
             Attributes.Add("condition", condition);
-            Attributes.Add("count", count.ToString());
-            Attributes.Add("interval", interval.GetSeconds());
+            Attributes.Add("count", count);
+            Attributes.Add("interval", interval);
             if (firstFastRetry != null)
             {
                 Attributes.Add("first-fast-retry", BoolMapper.Map(firstFastRetry.Value));
