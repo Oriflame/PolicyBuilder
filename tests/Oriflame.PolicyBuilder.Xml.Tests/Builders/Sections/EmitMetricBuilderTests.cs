28ae19d238d801a7206235253e3ad3f490472d76
Ondřej Škrabánek <ondrej.skrabanek@oriflame.com>
2021-12-15 08:57:10 +0000

    Test fix

--- tests/Oriflame.PolicyBuilder.Xml.Tests/Builders/Sections/EmitMetricBuilderTests.cs
+++ tests/Oriflame.PolicyBuilder.Xml.Tests/Builders/Sections/EmitMetricBuilderTests.cs
@@ -1,29 +1,29 @@
-﻿//using System.Collections.Generic;
-//using System.Linq;
-//using FluentAssertions;
-//using Oriflame.PolicyBuilder.Xml.Builders.Sections;
-//using Oriflame.PolicyBuilder.Xml.Definitions.Sections;
-//using Xunit;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Oriflame.PolicyBuilder.Xml.Builders.Sections;
+using Oriflame.PolicyBuilder.Xml.Definitions.Sections;
+using Xunit;
 
-//namespace Oriflame.PolicyBuilder.Xml.Tests.Builders.Sections
-//{
-//    public class EmitMetricBuilderTests
-//    {
-//        [Theory]
-//        [InlineData("foo", "foo-value", "foo-namespace")]
-//        public void CreatesCorrectPolicy(string name, string value, string @namespace)
-//        {
-//            var attributes = new Dictionary<string, string>()
-//            {
-//                { "name", name },
-//                { "value", value },
-//                { "namespace", @namespace }
-//            };
-                
-//            var basePolicy = new EmitMetricPolicyBuilder(attributes).Create() as SectionPolicy;
-//            var xml = basePolicy?.GetXml().ToString();
-//            xml.Should().Be(@$"<emit-metric name=""{name}"" value=""{value}"" namespace=""{@namespace}"" />");
-//        }
+namespace Oriflame.PolicyBuilder.Xml.Tests.Builders.Sections
+{
+    public class EmitMetricBuilderTests
+    {
+        [Theory]
+        [InlineData("foo", "foo-value", "foo-namespace")]
+        public void CreatesCorrectPolicy(string name, string value, string @namespace)
+        {
+            var attributes = new Dictionary<string, string>()
+            {
+                { "name", name },
+                { "value", value },
+                { "namespace", @namespace }
+            };
+
+            var basePolicy = new EmitMetricPolicyBuilder(attributes).Create() as SectionPolicy;
+            var xml = basePolicy?.GetXml().ToString();
+            xml.Should().Be(@$"<emit-metric name=""{name}"" value=""{value}"" namespace=""{@namespace}"" />");
+        }
 
 //        [Theory]
 //        [ClassData(typeof(EmitMetricsPolicyDataGenerator))]
@@ -50,5 +50,5 @@
 //    $@"  <dimension name=""{x.Name}"" value=""{x.Value}"" />").Aggregate("", (prev, curr) => $"{prev}{curr}\n") +
 //"</emit-metric>");
 //        }
-//    }
-//}
+    }
+}
