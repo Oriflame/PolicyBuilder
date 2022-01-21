9d799f0aef21983b09cde2ffff832c489254764b
Ondrej Skrabanek <ondrej.skrabanek@oriflame.com>
2022-01-21 13:04:45 +0000

    Trace new version

---
+++ src/Oriflame.PolicyBuilder.Policies/Builders/Fluent/Sections/ITracePolicyBuilder.cs
@@ -0,0 +1,9 @@
+﻿namespace Oriflame.PolicyBuilder.Policies.Builders.Fluent.Sections
+{
+    public interface ITracePolicyBuilder : IPolicySectionBuilder
+    {
+        ITracePolicyBuilder Message(string message);
+
+        ITracePolicyBuilder Metadata(string name, string value);
+    }
+}
---
+++ tests/Oriflame.PolicyBuilder.Xml.Tests/Builders/Sections/TraceBuilderTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Oriflame.PolicyBuilder.Xml.Builders.Sections;
+using Oriflame.PolicyBuilder.Xml.Definitions.Sections;
+using Oriflame.PolicyBuilder.Xml.Tests.Builders.Sections.Data;
+using Xunit;
+
+namespace Oriflame.PolicyBuilder.Xml.Tests.Builders.Sections
+{
+    public class TraceBuilderTests
+    {
+        [Theory]
+        [ClassData(typeof(TraceTestDataGenerator.CreatesCorrectFullPolicy))]
+        public void CreatesCorrectFullPolicy(TraceTestData data)
+        {
+            var attributes = new Dictionary<string, string>()
+            {
+                { "source", data.Source },
+                { "severity", data.Severity.ToString().ToLower() }                
+            };
+            var builder = new TracePolicyBuilder(attributes);
+            builder.Message(data.Message);
+            foreach (var m in data.Metadata)
+            {
+                builder.Metadata(m.Name, m.Value);
+            }
+            var basePolicy = builder.Create() as SectionPolicy;
+
+            var xml = basePolicy.GetXml().ToString();
+            xml.Should().Be(
+$@"<trace source=""{data.Source}"" severity=""{data.Severity.ToString().ToLower()}"">
+  <message>{data.Message}</message>
+" + data.Metadata.Select(x =>
+$@"  <metadata name=""{x.Name}"" value=""{x.Value}"" />").Aggregate("", (prev, curr) => $"{prev}{curr}{System.Environment.NewLine}") +
+"</trace>");
+        }
+    }
+}
