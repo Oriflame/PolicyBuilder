1ebbc205d9d81458f67f9a9992a8f9b08d83dd7b
Pavel Cermak <OSWPavelC@oriflame.com>
2022-03-29 13:36:07 +0200

    Add LimitConcurrency support for Backend section

--- src/Oriflame.PolicyBuilder.Policies/Builders/Fluent/Sections/IBackendSectionPolicyBuilder.cs
+++ src/Oriflame.PolicyBuilder.Policies/Builders/Fluent/Sections/IBackendSectionPolicyBuilder.cs
@@ -7,6 +7,9 @@ namespace Oriflame.PolicyBuilder.Policies.Builders.Fluent.Sections
 {
     public interface IBackendSectionPolicyBuilder : IPolicySectionBuilder<IBackendSectionPolicyBuilder>
     {
+        /// <see href="https://docs.microsoft.com/en-us/azure/api-management/api-management-advanced-policies#LimitConcurrency"/>
+        IBackendSectionPolicyBuilder LimitConcurrency(string key, int maxCount, Func<IBackendSectionPolicyBuilder, ISectionPolicy> action);
+
         /// <see href="https://docs.microsoft.com/en-us/azure/api-management/api-management-transformation-policies#SetBackendService"/>
         IBackendSectionPolicyBuilder SetBackendService(string url);
 
