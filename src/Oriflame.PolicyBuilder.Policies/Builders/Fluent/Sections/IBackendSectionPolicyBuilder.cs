b0f8aa65b484fe593d0e03db4783bf8aa61f929a
Pavel Cermak <OSWPavelC@oriflame.com>
2022-03-30 14:20:44 +0200

    Add Limit Concurrency string overload for backend section

--- src/Oriflame.PolicyBuilder.Policies/Builders/Fluent/Sections/IBackendSectionPolicyBuilder.cs
+++ src/Oriflame.PolicyBuilder.Policies/Builders/Fluent/Sections/IBackendSectionPolicyBuilder.cs
@@ -10,6 +10,9 @@ namespace Oriflame.PolicyBuilder.Policies.Builders.Fluent.Sections
         /// <see href="https://docs.microsoft.com/en-us/azure/api-management/api-management-advanced-policies#LimitConcurrency"/>
         IBackendSectionPolicyBuilder LimitConcurrency(string key, int maxCount, Func<IBackendSectionPolicyBuilder, ISectionPolicy> action);
 
+        /// <see href="https://docs.microsoft.com/en-us/azure/api-management/api-management-advanced-policies#LimitConcurrency"/>
+        IBackendSectionPolicyBuilder LimitConcurrency(string key, string maxCount, Func<IBackendSectionPolicyBuilder, ISectionPolicy> action);
+
         /// <see href="https://docs.microsoft.com/en-us/azure/api-management/api-management-transformation-policies#SetBackendService"/>
         IBackendSectionPolicyBuilder SetBackendService(string url);
 
