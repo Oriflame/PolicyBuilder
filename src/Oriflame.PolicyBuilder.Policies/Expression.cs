032e4320a7bbacdd1d2e712a108aa148e2627ffd
Ondřej Škrabánek <ondrej.skrabanek@oriflame.com>
2023-08-07 14:25:44 +0200

    Expression class added

---
+++ src/Oriflame.PolicyBuilder.Policies/Expression.cs
@@ -0,0 +1,43 @@
+﻿namespace Oriflame.PolicyBuilder.Policies
+{
+    public abstract class Expression
+    {
+        protected string Statement { get; set; }
+
+        protected Expression(string statement)
+        {
+            Statement = statement;
+        }
+
+        public static implicit operator string(Expression expression)
+        {
+            return expression.ToString();
+        }
+
+        public abstract string ToString();
+    }
+
+    public class SingleStatementExpression : Expression
+    {
+        public SingleStatementExpression(string statement) : base(statement)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"@({Statement})";
+        }
+    }
+
+    public class MultiStatementExpression : Expression
+    {
+        public MultiStatementExpression(string statement) : base(statement)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"@{{{Statement}}}";
+        }
+    }
+}
